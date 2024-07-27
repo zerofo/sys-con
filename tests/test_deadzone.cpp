9dd7ce7f89164bc9abd7ad32e7c493a4ef05ef27
o0Zz <oozz01@gmail.com>
2024-07-27 14:48:09 +0200

    Added deadzone tests

---
+++ tests/test_deadzone.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "Controllers/BaseController.h"
+
+TEST(UTILS, test_deadzone)
+{
+    // Deadzone 0%
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(0, 0.0f), 0.0f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(0, 0.2f), 0.2f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(0, 0.9f), 0.9f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(0, 1.0f), 1.0f);
+
+    // Deadzone 10%
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(10, 0.0f), 0.0f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(10, 0.05f), 0.0f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(10, 0.1f), 0.0f);
+    EXPECT_NEAR(BaseController::ApplyDeadzone(10, 0.11f), 0.01f, 0.01f);
+    EXPECT_NEAR(BaseController::ApplyDeadzone(10, 0.2f), 0.11f, 0.01f);
+    EXPECT_NEAR(BaseController::ApplyDeadzone(10, 0.9f), 0.88f, 0.01f);
+    EXPECT_FLOAT_EQ(BaseController::ApplyDeadzone(10, 1.0f), 1.0f);
+}
--- tests/test_normalize.cpp
+++ tests/test_normalize.cpp
@@ -3,18 +3,17 @@
 
 TEST(UTILS, test_normalize)
 {
+    //-32768, 32767
     EXPECT_FLOAT_EQ(BaseController::Normalize(-32768, -32768, 32767), -1.0f);
     EXPECT_FLOAT_EQ(BaseController::Normalize(0, -32768, 32767), 0.0f);
-
     EXPECT_FLOAT_EQ(BaseController::Normalize(32767, -32768, 32767), 1.0f);
     EXPECT_FLOAT_EQ(BaseController::Normalize(0, -32768, 32767), 0.0f);
-
     EXPECT_FLOAT_EQ(BaseController::Normalize(0, -32768, 32767), 0.0f);
     EXPECT_NEAR(BaseController::Normalize(-128, -32768, 32767), 0.0f, 0.01f);
 
+    // 0, 255
     EXPECT_NEAR(BaseController::Normalize(128, 0, 255), 0.0f, 0.01f);
     EXPECT_FLOAT_EQ(BaseController::Normalize(0, 0, 255), -1.0f);
-
     EXPECT_NEAR(BaseController::Normalize(109, 0, 255), -0.14f, 0.01f);
     EXPECT_FLOAT_EQ(BaseController::Normalize(0, 0, 255), -1.0f);
 }
